6ea7559ee6ab661bb8de9d11d4cf261075d0c11a
Gastón Konstantinides <gastonkons@gmail.com>
2023-03-16 00:58:10 -0300

    docs: delete unnecessary comments

--- api/src/models/spotifySearchHistory.ts
+++ api/src/models/spotifySearchHistory.ts
@@ -1,50 +1,5 @@
-import { Model, DataTypes } from 'sequelize';
-import { db } from '../database/db';
-
-// interface SpotifySearchHistoryAttributes {
-//   id: number;
-//   ip_address: string;
-//   artist_name: string;
-//   search_time: Date;
-// }
-
-// class SpotifySearchHistory extends Model<SpotifySearchHistoryAttributes>
-//   implements SpotifySearchHistoryAttributes {
-//   public id!: number;
-//   public ip_address!: string;
-//   public artist_name!: string;
-//   public search_time!: Date;
-// }
-
-// SpotifySearchHistory.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       autoIncrement: true,
-//       primaryKey: true,
-//     },
-//     ip_address: {
-//       type: DataTypes.STRING(15),
-//       allowNull: false,
-//     },
-//     artist_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     search_time: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     sequelize: db,
-//     tableName: 'spotify_search_history',
-//     timestamps: false, // Configuración para desactivar las propiedades de fecha
-//   }
-// );
-
-// export { SpotifySearchHistory };
-
+import { DataTypes } from 'sequelize'
+import { db } from '../database/db'
 
 const SpotifySearchHistory = db.define('SpotifySearchHistory', {
   id: {
